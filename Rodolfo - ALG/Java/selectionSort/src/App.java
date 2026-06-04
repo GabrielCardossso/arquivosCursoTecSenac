a33e09d28fe141336a6c94c366687eb60ee9ae9b
Gabriel Cardoso <gabrielcardossso@gmail.com>
2026-06-04 17:50:34 -0300

    feat: subindo novos arquivos

--- Rodolfo - ALG/Java/selectionSort/src/App.java
+++ Rodolfo - ALG/Java/selectionSort/src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) {
-        
+        int[] numeros = { 6, 9, 3, 10, 1, 7, 2, 8, 4, 5 };
+
+        SelectionSort selectionSort = new SelectionSort(numeros);
+
+        selectionSort.ordenarLista();
+
+        for (int numero : selectionSort.getArray()) {
+            System.out.print(numero + " ");
+        }
     }
 }
--- Rodolfo - ALG/Java/selectionSort/src/SelectionSort.java
+++ Rodolfo - ALG/Java/selectionSort/src/SelectionSort.java
@@ -1,2 +1,31 @@
 public class SelectionSort {
+
+    private int[] array;
+
+    public SelectionSort(int[] array) {
+        this.array = array;
+    }
+
+    public void ordenarLista() {
+        for (int i = 0; i < array.length - 1; i++) {
+
+            int menor = i;
+
+            for (int j = i + 1; j < array.length; j++) {
+
+                if (array[j] < array[menor]) {
+                    menor = j;
+                }
+            }
+
+            int temp = array[i];
+            array[i] = array[menor];
+            array[menor] = temp;
+        }
+    }
+
+    public int[] getArray() {
+        return array;
+    }
+
 }
